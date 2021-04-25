c276d4d260d57587cf8423648d0eaf972d99c0bd
matheus.geiser <matheus.geiser-ext@ab-inbev.com>
2021-04-25 15:39:35 -0300

    implementando fluxo com splash screen

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,16 @@
 import 'package:dev_quiz/core/app_gradients.dart';
 import 'package:dev_quiz/core/app_images.dart';
+import 'package:dev_quiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3)).then((_) => {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()))
+    });
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
