79d6d949023e6133832636ade4291a34b830e6de
matheus.geiser <matheus.geiser-ext@ab-inbev.com>
2021-04-25 18:54:31 -0300

    finalizando animações

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -49,7 +49,7 @@ class AppBarWidget extends PreferredSize {
                   ),
                   Align(
                     alignment: Alignment(0.0, 1.2),
-                      child: ScoreCardWidget()
+                      child: ScoreCardWidget(percent: user.score/100,)
                   )
                 ],
               ),
--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -4,6 +4,10 @@ import 'package:dev_quiz/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
 
 class ScoreCardWidget extends StatelessWidget {
+  final double percent;
+
+  const ScoreCardWidget({Key? key,required this.percent}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,7 +25,7 @@ class ScoreCardWidget extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
-              child: ChartWidget(),
+              child: ChartWidget(value: percent,),
             ),
             Expanded(
               flex: 3,
